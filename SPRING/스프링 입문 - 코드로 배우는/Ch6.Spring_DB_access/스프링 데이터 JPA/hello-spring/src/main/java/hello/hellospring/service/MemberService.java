b1e30a868feca74c75cbc4e617b9255e1cfd2062
yuseogi0218 <yuseogi0218@gmail.com>
2021-07-04 14:06:44 +0900

    SPRING

---
+++ SPRING/스프링 입문 - 코드로 배우는/Ch6.Spring_DB_access/스프링 데이터 JPA/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,47 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository; // 외부에서 넣어 주도록
+    }
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        //이름 중복 검사
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                } );
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return  memberRepository.findById(memberId);
+    }
+}
