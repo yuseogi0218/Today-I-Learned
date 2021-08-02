25103cb04d07967859ba4f0bfa11c15734961ac4
yuseogi0218 <yuseogi0218@gmail.com>
2021-08-02 19:34:51 +0900

    SPRING

--- SPRING/스프링 기본/Ch4. 스프링 컨테이너와 스프링 빈/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ SPRING/스프링 기본/Ch4. 스프링 컨테이너와 스프링 빈/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class BeanDefinitionTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    //xml
+    // GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
 
     @Test
     @DisplayName("빈 설정 메타 정보 확인")
