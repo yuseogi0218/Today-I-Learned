2193cc5559529f3d6e10d4fcd9099e31f3743608
yuseogi0218 <yuseogi0218@gmail.com>
2021-08-09 17:01:21 +0900

    SPRING

---
+++ SPRING/스프링 기본/Ch5. 싱글톤 컨테이너/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,27 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+    //저장소 객체 생성
+    private final MemberRepository memberRepository;
+
+    // 의존성 주입
+    //외부(AppConfig)에서 MemberRepository 의 구현 객체를 할당 해 준다.
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+}
