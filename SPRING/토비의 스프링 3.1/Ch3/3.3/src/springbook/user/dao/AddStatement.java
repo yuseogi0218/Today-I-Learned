2fd86d53aedf4aa74350794c7d61242328302bd3
yuseogi0218 <yuseogi0218@gmail.com>
2022-05-24 10:39:34 +0900

    spring 토비의 스프링 3.1 - 3.3.1

---
+++ SPRING/토비의 스프링 3.1/Ch3/3.3/src/springbook/user/dao/AddStatement.java
@@ -0,0 +1,28 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AddStatement implements StatementStrategy {
+    User user;
+
+    public AddStatement(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, password) values(?,?,?)");
+
+        // 그런데 user 는 어디서 가져올까?
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        return ps;
+    }
+}
---
+++ SPRING/토비의 스프링 3.1/Ch3/3.3/src/springbook/user/dao/UserDao.java
@@ -0,0 +1,125 @@
+package springbook.user.dao;
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import springbook.user.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void add(User user) throws SQLException {
+
+
+        StatementStrategy stmt = new AddStatement(user);
+        jdbcContextWithStatementStrategy(stmt);
+    }
+
+    public User get(String id) throws SQLException {
+        Connection c = this.dataSource.getConnection();
+        PreparedStatement ps = c
+                .prepareStatement("select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+
+        User user = null;
+        if (rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        if (user == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
+        return user;
+    }
+
+    /**
+     * User 테이블의 모든 데이터 삭제
+     */
+    public void deleteAll() throws SQLException {
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
+    }
+
+    /**
+     * User 테이블의 데이터 개수를 반환한다.
+     */
+    public int getCount() throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            c = this.dataSource.getConnection();
+
+            ps = c.prepareStatement("select count(*) from users");
+
+            rs = ps.executeQuery();
+            rs.next();
+
+            int count = rs.getInt(1);
+
+            return count;
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            }
+
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+
+                }
+            }
+
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+
+                }
+            }
+
+        }
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = dataSource.getConnection();
+
+            ps = stmt.makePreparedStatement(c);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {try {ps.close();} catch (SQLException e) {}}
+            if (c != null) {try {c.close();} catch (SQLException e) {}}
+        }
+    }
+}
